639079184099f205a0e8464f998372b760812d6a
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-02-14 20:29:26 +0530

    escape.py

---
+++ escape.py
@@ -0,0 +1,10 @@
+# Online Python compiler (interpreter) #to run Python online.
+''' Write Python# 3 code in this online editor and run it.#
+# print("iam a officer") '''
+"""gfvdggfe"""
+print('sarthak is a legend \n and he will become and\n AIR 1')
+print("sarthak ois curious person\n648\n")
+print("sarthak heaven is live want in gadipura\n and he want to rich as like not money but power")
+print("hey i am",64,32,sep="%\\n",end="54\n")
+print("sarthak is slient but justisable person@\n")
+print("sarthak favouriate rule is equallity<\n>justice")
