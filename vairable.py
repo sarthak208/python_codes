b113e9b9a3c2ed8373d3b89d06d12a8a0479b025
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-02-15 20:34:50 +0530

    vairable.py

---
+++ vairable.py
@@ -0,0 +1,28 @@
+a="43\n"
+print("a")
+b="army"
+print(b)
+c=65.53
+print(print(c))
+(e)="false"
+print(e)
+x='srushti'
+print(type(x))
+double=7557.8
+print(print(type(double)))
+p=q=r=73475
+print(p)
+w="65+32i"
+print(w)
+list1={54,3,32.65,90}
+print(list1)
+tuple5=(65,(972),9,72,11)
+print(tuple[3])
+t=[3,5,7,9]
+print(t[2])
+z=complex(3,8)
+print(complex(3,8))
+d1="my wish is want to become class 1 officer"
+print("the  type of d1 is ", type(d1))
+menu=['name:sarthak','id:6445','class:sy']
+print(menu)
