3da04b404e4268d3f16cc5028b4952dbd3899cd6
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-02-18 20:19:46 +0530

    Create Calculater.py

---
+++ Calculater.py
@@ -0,0 +1,8 @@
+a=54
+b=67
+print("the value of a+b is,a  is 54" ,"+ and b is 67, is",a+b)
+print("the value of a-b is,a  is 54" ,"- and b is 67, is",a-b)
+print("the value of a*b is,a  is 54" ,"* and b is 67, is",a*b)
+print("the value of a/b is,a  is 54" ,"/ and b is 67, is",a/b)
+print("the value of a%b is,a  is 54" ,"% and b is 67, is",a%b)
+print("the value of a//b is,a  is 54" ,"// and b is 67, is",a//b)
