b0860a9b73650b12278d0727b165c9a6af72f55d
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-02-18 19:56:44 +0530

    Create Typecast.py

---
+++ Typecast.py
@@ -0,0 +1,21 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+#  print("Try programiz.pro")
+a=653
+b=87
+print((a,b))
+print(sum)
+print(int(a)+int(b))
+print(a+b)
+print("a+b")
+n="indian army"
+print(str(n))
+# implict typecast
+g=65.98
+j=63
+print((g)+(j))
+# explict typecasting
+k=750
+s=int(k)
+print(k+s)
+print(k)
