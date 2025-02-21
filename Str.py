dffd57775ccd335d57f62283e2c65870e766c0da
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-02-21 21:10:20 +0530

    Create Str.py

---
+++ Str.py
@@ -0,0 +1,14 @@
+a="solider"
+print(a)
+b="sarthak  is a brave solider,\n  he is killed 100 pakistani"
+print(b)
+f='''hresihgejrh.5k5ykkphy50pp69y5ihklgfnl'''
+'''uit4l3yvtnno9tn93uyiojhoh;jlojbjbhghkf'''
+'''ilt547vt9t9yuo'''
+print(f)
+x=["akshay","salman,vijay,amir"]
+print(x[1])
+for i in x:
+    print(i)
+    v="sarthak is  AIR 120"
+    print(v+"\nhandsome person")
