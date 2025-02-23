7989e11111ab51eb563abaf1c90975e84e199850
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-02-23 19:27:52 +0530

    Create String_slice.py

---
+++ String_slice.py
@@ -0,0 +1,12 @@
+a="indian,\army\nlover"
+print(a)
+print(a[1:4])
+print(a[-2:-6])
+print(len(a))
+print(a[:])
+print(a[0:-len(a):-5])
+sj="harry"
+print(sj[-4:-2])
+def print_letter(string):
+    string ="shivaji raja"
+    print("\n".join(string))
