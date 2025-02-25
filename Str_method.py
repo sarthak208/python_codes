27503d6bad45ed24e38d94280ce399905d6f74a8
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-02-25 12:55:24 +0530

    Create Str_method.py

---
+++ Str_method.py
@@ -0,0 +1,32 @@
+a="!!??indian legend????"
+print(len(a))
+print(a.upper())
+#strings are immutable 
+# tuples are muttable
+print(a.lower())
+b='!indian army!!!!!!!!'
+print(b.rstrip("!"))
+print(a.replace("sarthak","IAS"))
+print(a.split("!"))
+print(b[1].upper())
+print(b[3].lower())
+blogheading="apj abdul kalam"
+print(blogheading.capitalize())
+str1="welcome to france"
+print(len(str1.center(265)))
+print(str1.center(134))
+print(str1.count("welcome to france"))
+print(a.endswith("for",54))
+print(b.find("ia"))
+y="8695i@#4srushti"
+print(y.isalnum())
+q="phIr hera pheri"
+print(q[3].isalpha())
+print(q.isalpha())
+s="gorkha is fearless troop in world"
+print(s.isspace())
+print(s.istitle())
+print(str1.islower())
+print(a.isupper())
+print(b.swapcase())
+print(a.title())
