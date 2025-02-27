3ed0cf2e3db2a0a284c8480bc6c6101850f86518
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-02-27 20:20:18 +0530

    Create If_else.py

---
+++ If_else.py
@@ -0,0 +1,12 @@
+import time
+timetable=t.strftime("%H:%M:%S")
+print(timetable)
+timetable=(t.strftime("%H"))
+input=(int("enter your hour"))
+print(input)
+if(12<="%H" and 0=="%H"):
+print("good morning sir")
+elif(12>="%H" and 0!="%H"):
+print("good eve ming sir")
+else(12=="%H" and "%H">=12):
+print("good night sir")
