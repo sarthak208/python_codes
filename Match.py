0de33c40ba123850498c6e86275fa04250621f91
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-02-28 16:41:41 +0530

    Create Match.py

---
+++ Match.py
@@ -0,0 +1,29 @@
+a=4
+int=input("enter value of a")
+print(a)
+Match a:
+      case 1:
+         print("first value")
+
+      case 2:
+          print("second value")
+
+      case 3:
+          print("third value")
+
+      case 4:
+         print("fourth value")
+
+case if_a<=4:
+     print("a is smaller than equal to")
+
+
+case else_a!=4 and a>=2:
+    print("a is not and a is greater than")
+
+
+case 5:
+   print("default value")
+
+case a_:
+   print(a)
