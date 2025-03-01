1eed3dd9c49b3c39dc02c899ec128af3a21aaece
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-03-01 16:26:17 +0530

    Create For.py

---
+++ For.py
@@ -0,0 +1,15 @@
+a="indian army"
+n="para sf"
+for n in a:
+  if a<=n:
+   print("a is smaller than n")
+  elif:
+    print("n is smaller than a")
+
+ s="German sheltered"
+   for s in m:
+    print(m[4])
+
+t=[45,23,56,89]
+ for t in range(89):
+    print(t)
