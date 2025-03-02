2e76d084cf7161b026c8af205100c1bee4881c97
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-03-02 13:47:29 +0530

    Create While.py

---
+++ While.py
@@ -0,0 +1,20 @@
+'''i=0
+while(i<=5):
+    print(i)
+i=i+1'''
+
+for i in range(5):
+    if  i<=7:
+        print("i is smaller than 7")
+    elif i==7:
+        print("i is equal to 7")
+        
+    print(i)
+    
+    i=0
+    while(i<=5  or i>=8):
+        print(i)
+        i=i+1
+        
+ int=input("enter value of i")      
+  print(int)
