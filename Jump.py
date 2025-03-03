52964f63d3bafee43ab1ae5baac4062fb8b720f5
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-03-03 10:19:19 +0530

    Create Jump.py

---
+++ Jump.py
@@ -0,0 +1,17 @@
+import hashlib
+i=6
+for i in range(6):
+    print(i)
+    while(i!=6):
+         break
+
+number = 0
+
+import math
+for number in range(10):
+    if number == 5:
+        continue    # break here
+
+    print('Number is ' + str(number))
+
+print('Out of\nloop')
