bd9e2ff4853f973d4fc03491723c7f6e4605a2e1
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-03-04 21:12:45 +0530

    Create Func.py

---
+++ Func.py
@@ -0,0 +1,28 @@
+'''def  calculateGmean(a=75,b=65):
+    Gmean=(a*b)/(a+b)
+     print(Gmean)'''
+
+     '''Gmean2(c,d)
+       c=90
+        d=30
+         Gmean2=(c+d)/(c*d)
+       print(Gmean2)'''
+       
+        def createcalculater(a,b,c,d):
+            count=0:
+            
+            if(a==1):
+                print("the value  of a is 1 and b is 5, "+",a+b")
+                
+           elif(a==b):
+                 print("the value  of a is 1 and b is 5, "-",a-b")
+                 
+            elif(c==b):
+                 print("the value  of a is 1 and b is 5, "*",a*b")
+                 
+            else;
+                 print("the value  of a is 1 and b is 5, "\", a\b")
+                 
+            for i  in range(4):
+                 while(count!=4)
+                  print(i)
