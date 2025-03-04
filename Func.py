892ed31ed8b41206b071f95dfac64e7172e4e44d
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-03-04 22:10:11 +0530

    Update Func.py

--- Func.py
+++ Func.py
@@ -1,28 +1,21 @@
-'''def  calculateGmean(a=75,b=65):
-    Gmean=(a*b)/(a+b)
-     print(Gmean)'''
+def calculater(a,b): 
+    a=54
+    b=76
+a=int(input("enter value of  a:"))
+b=int(input("enter value of  b:"))
 
-     '''Gmean2(c,d)
-       c=90
-        d=30
-         Gmean2=(c+d)/(c*d)
-       print(Gmean2)'''
-       
-        def createcalculater(a,b,c,d):
-            count=0:
-            
-            if(a==1):
-                print("the value  of a is 1 and b is 5, "+",a+b")
-                
-           elif(a==b):
-                 print("the value  of a is 1 and b is 5, "-",a-b")
+if("a==+b"):
+        print("the value of a  and b is","+",a+b)
+elif("a==-b"):
+                 print("the value  of a is 1 and b is 5", "-" ,a-b)
                  
-            elif(c==b):
-                 print("the value  of a is 1 and b is 5, "*",a*b")
+elif("a==*b"):
+                 print("the value  of a is 1 and b is 5", "*" , "a*b")
                  
-            else;
-                 print("the value  of a is 1 and b is 5, "\", a\b")
-                 
-            for i  in range(4):
-                 while(count!=4)
-                  print(i)
+else:
+       print("noone value matched")                 
+def calculater(a,b): 
+      for a in range(54):
+        while(a!=54 and b!=76):
+            print(a)
+            print(b)
