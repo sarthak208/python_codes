0c949de80f2f8ed43a73508e24df2a233e42e0f2
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-03-05 21:04:07 +0530

    Create Fun_methods.py

---
+++ Fun_methods.py
@@ -0,0 +1,24 @@
+def makeflim(a="pushpa 2",b="chava"):
+    a=(input("the value of a is:"))
+    print(makeflim(a,b))
+
+    print("the value of a and b is ","makeflim(a,b)")
+    
+    def makelist(indoor="chess",outdoor="kabbadi",onlinegame="pubg"):
+       # print(indoor="chess",outdoor="thugs of  war",onlinegame="free fire")
+       
+       print(indoor,outdoor,onlinegame)
+          #  makelist("chess","kabbadi","pubg"):
+          
+import statistics
+import hashlib
+
+et1 =(1,2,3, 3,4, 4, 4,5, 5,6)
+x=0
+
+print(et1[3]*x/2)
+x="itm colleage()"
+print("itm collegae()")
+
+
+print("Mean of given\n data set is % s" % (statistics.mean(et1)))
