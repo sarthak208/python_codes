9a5c1f760f77aa6ee3e63724560f37ff3fe6b90b
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-03-06 14:07:23 +0530

    Create List.py

---
+++ List.py
@@ -0,0 +1,25 @@
+import math
+sp=[64,6987,43.6,21_78,65*53,32,str("indian"),str('srushti'),'true',"petbull"]
+print(sp)
+print(type(sp))
+print(type(sp[4]))
+print(sp[2])
+print(sp[2],sp[4],sp[3])
+print(sp[-5:3])
+print(len)
+print(sp[6-3])
+print(sp[len(sp)-4])
+print(sp[1-2])
+
+if 89 in sp:
+    for sp in sp:
+        print(sp)
+    print("print sp")
+    
+print(sp[-3:-1])
+print(sp[:])
+print(sp[-3:-2:-1][-4:1])
+
+i=[6,79,53,90,1365]
+lst=["i*i for i in range(5) if%2==0"]
+  print("lst")
