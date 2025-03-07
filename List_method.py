e985cdcf59034fa8904922c73b16f60d37d2e5bc
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-03-07 15:14:29 +0530

    Create List_method.py

---
+++ List_method.py
@@ -0,0 +1,24 @@
+import math
+k=[53,"asmee",45.7,'false']
+k.append(2)
+Print(k.append(2))
+h=[426,61,89,100,416,44]
+h.reverse(4)
+Print(h.reverse(3))
+Print(h.index())
+s=["s3",56,45[5],21,66]
+Print(s.count(3))
+p=[1000,1257,4163,9961,444]
+y=[764,45,7]
+p[2]=y
+p.insert(45,7)
+Print(p.insert(45,7))
+n=[67,88,33,45]
+n=p+s
+Print(n)
+n.extend(p)
+print(n)
+sg=(64,92,58,16)
+for i in sg:
+    print(sg)
+    print(i)
