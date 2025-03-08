b0a919ee4538c677dd621e8895a9fb6ef6ca10eb
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-03-08 22:28:19 +0530

    Tuple.py

---
+++ Tuple.py
@@ -0,0 +1,24 @@
+import math
+tuple=[65,5.8,'true',9687]
+tuple[3]=90
+print(tuple[3])
+print(tuple)
+print(type(tuple),type)
+print(type(tuple),tuple)
+print(type(len))
+print(tuple[1:-2])
+print(type[1:-2])
+ if 65 in tuple:
+     print("no is tuple ")
+     
+ else:
+     print("no is not tuple")
+     
+ tup2=[65,8,889,52,78]
+tup2[3]=tup2[1:3:5]
+print(tup2)
+
+tup2=([75,87,72,79,100])
+print(tup2[:])
+print(tup2[-1:0])
+print(tup2[0:])
