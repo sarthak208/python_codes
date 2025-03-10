73a5906c9e23fb4c59f027a0148b1192714c5732
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-03-10 17:58:23 +0530

    Create Excresise3.py

---
+++ Excresise3.py
@@ -0,0 +1,14 @@
+import  pandas
+tuple1=("1.wich regiment is very fearless in army\n 2.who is most powerful person\n 3.who is very beautiful acterss")
+ans2=("1.gorkha\n vidut jamwal\n 3.disha  patni")
+print(tuple1)
+print(ans2)
+list1=("1.wich is highest mountain peak in india?\n 2.capital of india \n3.wonder in india")
+ans2=("1.kanchanjunga\n 2.delhi\n 3.agra")
+print(list1)
+print(ans2)
+amount=[658483,8220,10000,56527,1235,23000]
+   count=0
+    while(amount!=index[5]):
+        amount=amount+1
+        print(amount)
