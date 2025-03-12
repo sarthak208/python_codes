5c83c533fe04f2a972901659cf6ad45dd1852976
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-03-12 12:56:58 +0530

    Create Docstring.py

---
+++ Docstring.py
@@ -0,0 +1,16 @@
+def factorial(n):
+   '''the value of given factorial and returns the 
+     given value'''
+    n=4
+    print(n*n-1)
+if n>=n-1:
+     print("n is greater than it's fact")
+elif:
+  print("n is smaller than fact)
+else:
+print("noone is smaller than fact")
+return 8
+square(8)
+
+print(square(8).__doc__)
+print(square.__docstrings__)
