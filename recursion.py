d33bc35a83bdf2d12b490f4f2a639f47c0a96f7d
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-03-13 18:47:49 +0530

    Create recursion.py

---
+++ recursion.py
@@ -0,0 +1,42 @@
+import math
+def recursion(x):
+'''if(x==1 or x==0):
+for x in range(4):
+    print(recursion(x))
+     return x:'''
+#else:
+#elif:
+# return x*recursion(x-1)
+print(recursion(4))
+print(recursion(3))
+print(recursion(2))
+factorial(5)=5*4*3*2*1
+factorial(4)=4*3*2*1
+factorial(3)=3*2*1
+factorial(2)=2*1
+
+def factorial(x):
+    while(x!=0):
+        print(factorial(5))
+        print(factorial(4))
+        print(factorial(3))
+        print(factorial(2))
+        print(factorial(1))
+      
+      def recursion(s):
+          sum=0:
+           If(0==+1):
+                return sum(0):
+      elif:
+           return sum(0)*(0+1):
+elif:
+  return sum(0)*(0+1)*(0+1+1):
+elif:
+ return sum(0)*(0+1)*(0+1+2):
+else:
+ return n*n-1:
+
+print(factorial(0))
+print(factorial(1))
+print(factorial(2))
+print(factorial(n*n-1))
