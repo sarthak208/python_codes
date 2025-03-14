13a5f844f671e49b272d1a6c722d02a2c1df7bf6
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-03-14 21:36:36 +0530

    Create Set.py

---
+++ Set.py
@@ -0,0 +1,23 @@
+import math
+s={674,92,65.49,'true','[634]'}
+data={75,92,55,20}
+while(data!=0):
+print(data)
+print(s)
+d=set({47,92,51.8,'false'})
+for d in d:
+    print(d)
+print(d)
+print(type(d))
+import pandas
+n={65,83,23.8,'true',"asmee"}
+while(n):
+  print(n)
+if(n==[4]):
+    print("the value is n")
+    
+elif(n!=[4]):
+    print("the value  is not n")
+    
+else:
+    print("nothing value")
