f0380ac152e25baed4ad901d0d20eec7438ba914
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-03-15 20:07:36 +0530

    Create Set_method.py

---
+++ Set_method.py
@@ -0,0 +1,66 @@
+import math
+
+
+# create a set of integer type
+student_id = {112, 114, 116, 118, 115}
+print('Student ID:', student_id)
+
+# create a set of string type
+vowel_letters = {'a', 'e', 'i', 'o', 'u'}
+print('Vowel Letters:', vowel_letters)
+
+# create a set of mixed data types
+mixed_set = {'Hello', 101, -2, 'Bye'}
+print('Set of mixed data types:', mixed_set)
+# create an empty set
+empty_set = set()
+
+# create an empty dictionary
+empty_dictionary = { }
+
+# check data type of empty_set
+print('Data type of empty_set:', type(empty_set))
+
+# check data type of dictionary_set
+print('Data type of empty_dictionary:', type(empty_dictionary))
+numbers = {21, 34, 54, 12}
+
+print('Initial Set:',numbers)
+
+# using add() method
+numbers.add(32)
+
+print('Updated Set:', numbers) 
+companies = {'Lacoste', 'Ralph Lauren'}
+tech_companies = ['apple', 'google', 'apple']
+
+# using update() method
+companies.update(tech_companies)
+
+print(companies)
+
+# Output: {'google', 'apple', 'Lacoste', 'Ralph Lauren'}
+languages = {'Swift', 'Java', 'Python'}
+
+print('Initial Set:',languages)
+
+# remove 'Java' from a set
+removedValue = languages.discard('Java')
+
+print('Set after remove():', languages)
+fruits = {"Apple", "Peach", "Mango"}
+
+# for loop to access each fruits
+for fruit in fruits: 
+    print(fruit)
+# first set
+A = {1, 3, 5}
+
+# second set
+B = {0, 2, 4}
+
+# perform union operation using |
+print('Union using |:', A | B)
+
+# perform union operation using union()
+print('Union using union():', A.union(B))
