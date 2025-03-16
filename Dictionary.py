62b5d9525a1030f0762ab6691e9827ebc2ac0a2a
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-03-16 20:41:08 +0530

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,42 @@
+import pandas
+dict2 = {74: "disha",
+54.93: "sreelela",
+3.90: "katrina",
+7812: '4386',
+'team':'army'
+    
+}
+x=4
+def_dict2(x):
+match_x:
+
+      case 1:
+          print("the value is 74")
+          break:
+              
+      case 2:
+          print("the value is 54.93")
+          break:
+              
+      case 3:
+          print("the value is 3.90")
+          break:
+              
+      case 4:
+          print("the value is 7812")
+          break: 
+              
+print(dict2)
+print(dict2[74])
+#d2 = dict(a = 'true', b = '8756', c = "Geeks")
+#print(d2)
+crop3={
+    'army':"indian army",
+    id:'874'
+}
+print(crop3.get('army'))
+print(crop3.keys())
+a=crop3.values()
+print(a)
+for key in crop3.keys():
+    print(f"the value of  corrsponding key[{id,'army'}]")
