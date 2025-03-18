ec6769cf75d37ec10c318dad6842ea44ac2cc1a1
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-03-18 17:17:12 +0530

    Create For_else.py

---
+++ For_else.py
@@ -0,0 +1,45 @@
+import hashlib
+str=4
+while(str==0):
+        print(str)
+
+str={(65.8,73,81,100,134)}
+#str2={[65.8,73,81,100,134]}
+#print(str2)
+
+for i in str:
+    for i in range(4):
+           # print(str)
+        print(i)
+        if str==134:
+            continue
+            print("str is correct")
+            
+   str=4:
+   match(str):
+       case 0:
+           print("the value of 1st is 65.8")
+           break
+       
+       case 1:
+         print("the value of 2nd is 73")
+           break
+       
+       case 2:
+         print("the value of 2nd is 81")
+           break
+       
+        case 3:
+         print("the value of 2nd is 81")
+           break
+       
+        case 4:
+         print("the value of 2nd is 134")
+           break
+       
+        case 5:
+         print("the value of 2nd is 100")
+           break
+       
+       default:
+           print("nothing case")
