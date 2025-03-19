b710c7d196dc1645b7fb758e06f82895a283f6d0
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-03-19 20:54:29 +0530

    Create exception.py

---
+++ exception.py
@@ -0,0 +1,48 @@
+import math
+import pandas
+import hashlib
+a=34
+a=input("enter value of a")
+while(a!=34):
+print("the value will be 34")
+b=15
+b=input("enter value of b")
+while(b!=15):
+print("the value will be 15")
+
+try:
+    a=34
+    res = 34 / b
+    
+except arthimetic error:
+    print("You can't divide by zero!")
+    
+except ValueError:
+    print("Enter a valid number!")
+    
+else:
+    print("Result is", res)
+    
+finally:
+    print("Execution complete.") 
+
+a=2
+match_a(num):
+ a=34
+ b=15
+    case 1:
+      if(a<=b):
+        break
+     print("b is greater than a")
+
+    case 2:
+      if(a>=b):
+       break
+print("a is greater than b")
+
+default:
+print("noone will excuate")
+
+      
+       
+   
