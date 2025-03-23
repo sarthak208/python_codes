708f473d248806fa790c62df95c475e0d75ec2e2
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-03-23 22:23:16 +0530

    Create Short_if.py

---
+++ Short_if.py
@@ -0,0 +1,44 @@
+import math
+a=41
+b=819
+if(a<b and b>a) print("a is greater than b and b is greater than a")
+else:print("a is not greater than b and b is not greater than a")
+while(a!=b):
+print("a and b is not number")
+
+try:
+print("the value of a is (41) and b is (819))
+
+except:
+print("another way")
+
+else:
+print("anyone way")
+
+finally:
+print("anyone method solve it")
+
+c='''the q a and b will be'''
+print(c.__docstring__)
+
+def math(a,b):
+print("all value a and b")
+
+  for a in range(41):
+     if(a<b):
+        break
+
+   for b in range(819):
+       if(b>a):
+          break
+       print("a is smaller than b")
+       print("b is smaller than a")
+
+    for a in a:
+        print(a)
+ else:
+   print("both not")
+
+  def_fact(a,b):
+    print(factorial(41))
+   print(factorial(819))
