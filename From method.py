624aaa88b1c844b49eea7167bcc314e55f1a248a
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-04-23 18:24:04 +0530

    Create From method.py

---
+++ From method.py
@@ -0,0 +1,29 @@
+import math
+from math import sqrt
+math.floor(764.8292)
+print(math.nan,type(math.nan))
+from math import sqrt,pi
+import math as s 
+import math as math_builtin_python
+math_builtin_python=744
+print(math_builtin_python)
+print(dir(math_builtin_python))
+print(type(math_builtin_python))
+f=s.sqrt(4)
+while(f!=4):
+    print("enter this value of f")
+    f=4
+    for f in range(4):
+        print(f)
+ans='''f the value of entering  this ans'''
+print(ans)
+print(__docstring__.ans)
+x=sqrt(13) * pi
+ans=math.sqrt(413)
+  
+print(ans)
+print(x) 
+if(f==4):
+       print("all value is clear")
+else:
+     print("all is not clear")
