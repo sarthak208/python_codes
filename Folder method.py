e7ce8e3bb7f67cdde999d7acd6fd6078620a2ea1
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-04-23 18:22:04 +0530

    Create Folder method.py

---
+++ Folder method.py
@@ -0,0 +1,20 @@
+import math
+def welcome():
+   math.welcome.mkdir(764)
+   folders=math.listdir(764)
+   math.mkdir(f"the value of {a+1}")
+   for a in range(0,764):
+       math.mkdir(f"764{2}")
+       for folders  in folders:
+           print(folders)
+           print(math.listdir(f"value/{folder}"))
+           math.system(3)
+           
+if(math==(764)):
+    print(f"new  directory  use")
+
+else:
+    print("failed  old version")
+    
+while(math!=764):
+    print(math)
