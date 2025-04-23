562d4e153176d76b3eccb986c83401c7d59f57ed
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-04-23 17:14:06 +0530

    Create Is.py

---
+++ Is.py
@@ -0,0 +1,51 @@
+import math
+a=5
+x=63.90
+print(a is x)
+print(x is not a)
+print(a)
+print(int(a)+int(x))
+while(a==x):
+    print("a will be x")
+    for x in range(a):
+        if x<a:
+            print("x is smaller than a")
+               break
+            
+            else:
+                print("a is smaller than x")
+                   continue
+                   
+           x=input("enter the called of x")
+              print(x)
+              
+              u="indian army"
+              s="legend"
+              print(u)
+        e=[52,91,1000,637,10]
+        print(e[1:2])
+        print(e[-1-4])
+              
+              def factorial(x):
+                  if(x==a and a==x):
+                      return x
+                      print(fact(x))
+              for a in a:
+                  print(a)
+                  
+               else:
+                   print(fact(a))
+                   
+                 def math(a):
+                     a=5
+               def choose(x):
+                 x=61
+                 print(x)
+                 print(a)
+           if __choose__=="a"
+              print(a)
+              
+              def custom(w):
+                  lambda w:custom w:
+                       print(w)
+               print(a==w)                     
