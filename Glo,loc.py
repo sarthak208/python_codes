80f69b3aeccf8f15e4fd075bf8ad6c6d81f4a776
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-04-23 18:21:15 +0530

    Create Glo,loc.py

---
+++ Glo,loc.py
@@ -0,0 +1,38 @@
+import math
+y=41
+def ota():
+    x=89
+    print("the value of x is",ota)
+    
+def ima():
+    c=58.90
+    print("the value of y is",ima)
+    
+def y():
+ y=41
+ print("inside y",y)
+         
+ota() 
+print("global:",ota)
+ima()        
+print("global:",ima)
+y()
+print("global:",y)
+ 
+for y in range(41):
+    if(y==41):
+        print("y is accurate")
+      
+        
+    else:
+        print("value is not correct")
+            
+        while(y!=41):
+            print(y)
+try: 
+     print("check all value")
+except y as e:
+    print("Define term")
+finally:
+    print("final result")         
+    print(y)
