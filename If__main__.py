b492b7686f2c041aefdfe927078c62e30d7ba88e
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-04-23 18:22:57 +0530

    Create If__main__.py

---
+++ If__main__.py
@@ -0,0 +1,30 @@
+import math
+def welcome():
+    print("welcome to the imagica water park")
+    math.welcome(a=4)
+    welcome(a)
+    a.welcome()
+    
+    if __name__== "__welcome__":
+        main.math()
+        welcome.main.math(a)
+        
+        for a  in  range(4):
+            if __name__=="__welcome__(a==0)":
+                print("the value  of a is 0")
+                print("other value ")
+                while(a!=0):
+                    a=f"the  value of given {2}term that will suffecirnt {4}"
+                    a=input("enter value of a")
+                    print(a)
+                    welcome.a()
+                    
+                a = ["Python", "Exceptions", "try and except","hydrabad","mumbai","kolkata"]  
+try:  
+     for i in range(4):  
+        print( "The index and element from the array is", i, a[i] )  
+except:  
+    print ("Index out of range")  
+    
+finally:
+    print("other way")
