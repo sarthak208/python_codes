93ba551372d0485738f6d0f5c300d45490cf27d4
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-04-24 17:53:54 +0530

    Create Exercise 5.py

---
+++ Exercise 5.py
@@ -0,0 +1,30 @@
+import math
+ans=["s, w, g"
+      "d,m,l"
+       "l,d,w"
+        "w,l,d"]
+def func1():
+ if("2=>1 and 0<=2"):
+     breakpoint()
+     computer = [0, 1, 2]
+
+
+def func2():
+ if("2<=1 and 0>=1"):
+    breakpoint()
+computer2=[3,4,5]
+
+
+def func3():
+  if("2<=1 and 0>=1"):
+    breakpoint()
+computer3=[6,7,8]
+
+print("the  gun  beats snake")
+print("the  water  beats gun")
+print("the  snake  beats water")
+
+def check(x):
+  x1=input("0 is  snake 1 is water and 2 for gun ")
+
+print(input())
