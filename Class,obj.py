46027e25ec64ced298d83c4cee51780a0c54bd17
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-04-28 18:16:49 +0530

    Create Class,obj.py

---
+++ Class,obj.py
@@ -0,0 +1,132 @@
+from traceback import print_tb
+imort math
+class gf: 
+freind ="srushti"
+age=20
+color="fair"
+gf1=gf()
+print(gf.__name__)
+print(gf.age)
+print(gf.color)
+def __init__(self, name, age, color):
+self.name=name
+self.age=age
+self.color=color
+print(())
+
+for gf1 in range(4):
+print (f"the value of")
+print(f" the value of")
+name=20
+for name in ange(2):
+if name=20:
+the fuction to {[3]}")
+the fuction to {(3)}")
+break
+else:
+break
+print(names())
+print("not gf1 only name()")
+try:
+print tb(nter 1st gf")
+except:
+print("other ways")
+finally:
+print("no way")
+#1 wat to become pytho devlpor also joi cds air 10
+'''i wat to chage costitutio to reduce reseravatio ad low as like saudi arab lawa d reduce idz'''
+populatio
+gf="she is very preety \nand taleted
+print(gf)
+a=6754
+b=86.96
+C=21
+z=928496
+print(in a)+int(z))
+
+print(a+b*2)
+d=input("enter the value of d ")
+print(d)
+print(input())
+
+a=0754
+b=86.96
+c=21
+55
+56
+
+z=928496
+print (a+b*c-Z)
+print (int(a)+int(z))
+d=input("enter the value of d: ")
+print(d)
+print(input())
+p="sarthaks family is very strog"
+print(p[1])
+worklen =len(" Lord vishu)
+print (worklen)
+print(p[3:-6])
+while(p!=0):
+print("p will be always correct")
+def fuc(q=532): 
+return q
+print(q)
+fuc()
+x={name:"ima" ,name2: "ota"])
+print(x)
+
+for x in x:
+print(x)
+else:
+print("enter other x")
+for x in euamrate (x):
+if x==0:
+print(x)
+def magic (w):
+W=9798
+if w==9798: print('value is covertable")
+else: print("the value is not convertable *)
+def cosuam(s):
+S=875
+print(s)
+print (w)
+def cake (y=756):
+cake: Lambda:y
+print(y)
+if cake__==__cake__ ():
+print('i went to make cake for asmee")
+
+else:
+print("enter other x")
+for x in euamrate(x):
+if x==0:
+print(x)
+def magic (w):
+W=9798
+if w==9798: print("value is covertable")
+print(s)
+print(w)
+else: print("the value is not convertable ")
+def cosuam(s):
+S=875
+def cake (y=756): 1 usage
+cake: lambda:y
+print(y)
+if cake__==cake():
+else:
+print("i want to make cake for asmee")
+print ("other person will cake bake")
+print(w)
+def cake (y=756): 
+cake: lambda:y
+print(y)
+if cake ==cake():
+print("i want to make cake for asmee")
+else:
+print("other person will cake bake")
+new_cake="my_ew_cake"
+math.mkdir(new_cake)
+if math.path.exists(new_cake):
+print (f"{"new_cake will form"}")
+else:
+print(f"{"other_cake will form"}")
