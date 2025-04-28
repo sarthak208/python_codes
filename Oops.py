7e0aec8f36ffdde3e3b0c15f45039b65908f9627
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-04-28 14:18:02 +0530

    Create Oops.py

---
+++ Oops.py
@@ -0,0 +1,49 @@
+import math
+print("legend para sf")
+
+math()
+x1=736
+d1="nation first"
+t1="uniform civial code"
+
+for x1 in range(736):
+    print(x1)
+
+if(x1):
+breakpoint()
+print("x1 is less than d1")
+
+else:
+print("x1 more than d1")
+
+while(math!=0):
+print("x1 is better")
+
+'''all [x1] and other term [e1] by the changing 
+values [t1]'''
+print(f"[math()]")
+x2=(5263)
+d2=["enemy kill"]
+t2=("reservation cancellation")
+
+math=input("enter the value is 0")
+print(math)
+
+try:
+print("using x1 method")
+
+except:
+print("enter d1")
+
+finally:
+print("enter t1")
+
+print(x2)
+print(t2)
+print(d2)
+
+if(x2==0):print("the value is 0")
+else:print("the value is not 0")
+
+
+
