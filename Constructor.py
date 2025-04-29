7a64f3233adb66719f25944cbc5fcc3f31c2322a
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-04-29 21:11:59 +0530

    Create Constructor.py

---
+++ Constructor.py
@@ -0,0 +1,135 @@
+from idlelib.colorizer import matched_named_groups
+from pickletools import string1
+from traceback import print_tb
+import math
+class family:
+      def __init__(self):
+
+          self.mentor="elder bro"
+          self.god="dad"
+          self.world="mom"
+          self.welwhisher="sister"
+
+family=family()
+print(family.mentor)
+print(family.god)
+print(family.world)
+print(family.welwhisher)
+
+class freinds:
+    def __init__(self,s1,s2,s3,s4):
+        self.s1="sager"
+        self.s2="shridhar"
+        self.s3="shrikant"
+        self.s4="sumit"
+
+freinds=freinds("school","colleage","house","town")
+print(freinds.s1)
+print(freinds.s2)
+print(freinds.s3)
+print(freinds.s4)
+
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand  # Set instance attribute
+        self.model = model  # Set instance attribute
+
+    def display(self):
+        return self.brand, self.model
+
+# Create an instance of Car
+car1 = Car("Toyota", "Corolla")
+
+# Call the display_info method
+print(car1.display())  # Output: This car is a Toyota Corolla
+
+class city:
+    def __init__(self,topic,wetaher):
+        self.topic="srucutre"
+        self.weather="condition"
+
+def topic(self="pune"):
+   return topic
+
+def weather(self="hot"):
+       return weather
+
+for family in range(4):
+    print(family)
+
+while(car1==1):
+    print("car1 is secure")
+
+while car1==car1:
+    print(car1)
+
+else:
+    print("use another case")
+
+#i will try  my 100%
+'''there is my nation is indiaa nd i want to join cds ima with\n air 1'''
+t=785.986
+z=86
+x=8900
+h=143
+print(t)
+print(z)
+print(int(t)+float(z))
+print(t-z*x%h)
+
+def sum(a,b):
+    return sum(a=75,b=4)
+print(sum)
+print(f"the value of {t} is[2]  and {x} will be [1]")
+print(sum.__doc__)
+
+z=input("enter value of z")
+print(z)
+str={65,87.95,5300}
+print(str)
+tuple2=(76,97,75)
+print(tuple2[1])
+arr=[75,9997,7575,721]
+print(arr)
+
+if(sum==a):
+    breakpoint()
+
+else:
+    breakpoint()
+print("value is a")
+print("value is b")
+
+for z in z:
+    print(z)
+else:
+    print("for but other  value")
+try:
+    print("use z")
+except:
+ print("usetuple2")
+
+finally:
+    print("no  other value")
+
+def result(c):
+ matched_named_groups(c)
+case_a:
+print("a is ans")
+case_b:
+print("b is ans")
+case_:
+print("noonw is apply")
+if(a<=b or a>=b):
+    raise arthimaticerror("thevalueshouled  be aaor b")
+print(t is z)
+print(t==x)
+
+# Python program to execute
+# main directly
+print ("Always executed")
+
+if __name__ == "__main__":
+ print ("Executed when invoked directly")
+else:
+ print ("Executed when imported")
