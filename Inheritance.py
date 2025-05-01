7949bc42d442da44efe92b138d0eeec030698ae1
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-05-01 16:20:14 +0530

    Create Inheritance.py

---
+++ Inheritance.py
@@ -0,0 +1,13 @@
+class officer:
+  def __init__(self,name):
+      self._name=name
+
+def show(self):
+   pass
+
+class solider(officer):
+        def display(self,name):
+             return f"{self._name}"
+
+officer=solider("sumit")
+print(officer.display("self,name="))
