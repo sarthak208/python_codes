31802e233d4b5c1d30f6a8a55662faf908bf30fe
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-05-02 16:22:30 +0530

    Create Access modifier.py

---
+++ Access modifier.py
@@ -0,0 +1,92 @@
+From codecs inport namereplace_errors
+fon enum inport nonnenber
+import math
+#public modifier
+class worker: 
+# constructor
+def __init__(self, name, age):
+# public data members
+self.workerName = name
+self.worker Age
+= age
+# public member function
+def displayAge (self, age): 
+print("Age: ", self.workerAge)
+def displayname(self, name):
+print("name: ", self.workername)
+# creating object of the class
+obj = worker( name "R2J" age: 20)
+# finding all the fields and methods which are present inside obj
+print("List of fields and methods inside obj, dir(obj))
+
+# accessing public data member
+print("Name:", obj.workerName)
+obj.displayAge (age-54)
+#protected modifier
+class social:
+_name=nonmember
+-year nonmember
+_nation=nonmember
+def __init__(self, name, year, nation):
+self._name=name
+self._year-year
+self._nation-nation
+def _showdata(self, name year,nation):
+print("the value of acess ndifier is self._name}")
+print("the value of acess mdifier is (self._year}")
+print("the value of acess mdifier is (self._nation}")
+
+class solider (social):
+init__(self, name, year, nation):
+social. init__(self, name, year, nation)
+def showdata (self):
+print('name:(self._name}")
+obj=object()
+print(obj)
+#private modifier
+class actress:
+_height=nonmember
+_weight=nonmember
+def __init__(self, height, weight):
+self._height-height
+self._weight-weight
+def showdetails(self):
+a=input("enter the heightof val")
+b=input("enter the weight of val")
+print("height: the value of given:")
+print("weight: the value of given: ")
+def accessprivatemodifier (self):
+self._showdetails()
+c=input("enter the val of
+print(c)
+showdetails()")
+i want to become cds AIR 1 and aslo ni will prove that!!!
+d=543
+e=986
+f=8954
+
+print(int(d)+int(e))
+print(int(d)*int(e)-int(f))
+for d in range(543):
+if d==e:
+break
+
+else:
+print("other val")
+
+try:
+def solve(g):
+g=input("enter the value of g")
+except:
+print("the value of g")
+finally:
+print("other method")
+if d!=e: print("the val is not ewal to")
+else: print("the valu is equal to")
+while e==0:
+print("use complte val")
+if __name__=="main":
+else:
+print("other values")
+def use (u=785):
+print (f"use().__doc__ thevalue store {4} and {2} term")
