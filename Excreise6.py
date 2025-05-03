a33d363dd5c9bda97f98a028b48b699bb3007b5a
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-05-03 15:32:17 +0530

    Create Excreise6.py

---
+++ Excreise6.py
@@ -0,0 +1,15 @@
+class library: 
+no_of_book1="raise of god"
+no_of_book2="king of thunder"
+def __init__(self, s1, s2):
+self.s1="text will be book1"
+self.s2="text will be book2"
+def showbook(self, book1, book2): 
+self.book1={f" self.s1"}
+self.book2={f"self.s2"}
+def addbook (self):
+print (f"the value of all the addbook.__doc__()")
+s1=library(s1="book1", s2="book2")
+s1.showbook (book1="raise of god", book2="king of thunder")
+s1=input(s1.no_of_book1)
+print(s1)
