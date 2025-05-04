93290783011bdf52846f028d34d74f963d0b6f30
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-05-04 20:13:00 +0530

    Create Excreise7.py

---
+++ Excreise7.py
@@ -0,0 +1,10 @@
+import os
+folders=("clutter.png")
+i=1
+folders=os.mkdir(i=i+1)
+for files in range(n):
+if files.endswith(".png"):
+i=input("enter the value of .png")
+i=1
+i=i+1
+print(files.rename(".png"))
