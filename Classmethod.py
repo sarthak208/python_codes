9695ab6090a5891cdedd0f7e555848e3edbf6e35
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-05-07 22:07:26 +0530

    Create Classmethod.py

---
+++ Classmethod.py
@@ -0,0 +1,196 @@
+from math import sqrt
+print(sqrt)
+import math
+import hashlib
+print(math)
+print("python is top\nmost code")
+#52783nfhd
+''' i will forevery love in my all parents
+family and welwhishers and my lovers and crushes'''
+
+try:
+    print("use 1st modules")
+
+except value as hashlib:
+    print("use other")
+    
+finally:
+    print("no other")   
+    
+    a="king sarthak"
+    b="queen srushti"
+    print(str(a)+str(b))
+   
+if(a==b):
+     print(a)  
+     
+else:
+     print("no other only default") 
+     
+c=input(a)
+print(c)
+name="mantri nager is real hail"
+print(name+name[4])
+print(name[10])
+print(name[5:-13])
+print(name.startswith("boobs"))
+print(name.endswith("dangerous"))
+print(name.istitle())
+print(name.swapcase())
+
+
+       
+def greet(person):
+    match person:
+        case "mahesh":
+            print("Hello, A!")
+        case "anurag":
+            print("Hello, B!")
+        case "prathmesh":
+            print("Hello, c!")
+        case _:
+           print("others")   
+
+greet("A")
+greet("B")
+greet("c")
+
+for A in a:
+    break
+    print(a[2])
+for a in range(9):
+    continue
+else:
+    print("no other use")  
+    
+print(a)        
+ 
+if A!=a:print("will be B") 
+else:print("will be c")
+
+def terriost(n):
+    if n == 0:return 1
+    else:return n * terriost(n-1)
+
+print(terriost(5))
+
+w='''the value of all Pahlgam attack there is terriost to kills 70'''
+print(f"terriost.__doc__ the value of w is {4} is 42 and 6 is {7}")
+
+lambda: w*5
+print(w*5)
+print(w is 10)
+print(w is not 10)
+print(w==5)
+
+ans = {1: "manmad", 2: "csmt terminus", 3: "latur road"}
+ans.clear()
+print(ans)
+d2 = dict(x = "Geeks", y = "for", z = "Geeks")
+print(d2.get(a))
+d = {'Name': 'Ram', 'Age': '19', 'Country': 'India'}
+print(list(d.items())[1][0])
+print(list(d.items())[1][1])
+print(d.keys())
+d2.update(d)
+print(d2.update())
+print(d.values())
+d = {'Name': 'Ram', 'Age': '19', 'Country': 'India'}
+d.pop('Age')
+print(d)
+
+d = {'Name': 'Ram', 'Age': '19', 'Country': 'India'}
+val = d.popitem()
+print(val)
+
+if "__name__"=="__main__":
+    print("print d ")
+    
+else:
+    print("other")    
+    
+    def sqrt(m=89):
+        return m
+        
+def term(o):
+ o=61
+ print(o)
+ 
+ m=input("enter value of m")   
+ print(m)
+ class Car:
+    def __init__(self):
+
+        #Initialize the Car with default attributes
+        self.make = "Toyota"
+        self.model = "Corolla"
+        self.year = 2020
+
+# Creating an instance using the default constructor
+car = Car("toyota","corolla",2020)
+print(car.make)
+print(car.model)
+print(car.year)
+
+class transfer:
+    def __init__(self):
+        self.byair="byair"
+        self.bycar="bycar"
+        self.byroad="byroad"
+        
+transfer=transfer()
+print(transfer.byair)
+print(transfer.bycar)   
+print(transfer.byroad) 
+ 
+def repair(transfer):
+    def __init__(self):
+         self.work=work
+         self.industry=industry
+         self.medical=medical
+         
+    
+def printdetails(self,work,industry,medical):
+        print("use that case")   
+        
+reapir=repair()
+repair.printdetails():   
+
+def biology():
+    cell="rbc" 
+    cell2="wbc"  
+    name5="cellulose"
+    
+    for cell in enumerate(2):
+        print(cell)
+    
+class Geeks:
+    course = 'DSA'
+    list_of_instances = []
+
+    def __init__(self, name):
+        self.name = name
+        Geeks.list_of_instances.append(self)
+
+    @classmethod
+    def get_course(cls):
+        return f"Course: {cls.course}"
+
+    @classmethod
+    def get_instance_count(cls):
+        return f"Number of instances: {len(cls.list_of_instances)}"
+
+    @staticmethod
+    def welcome_message():
+        return "Welcome to Geeks for Geeks!"
+
+# Creating instances
+g1 = Geeks('Alice')
+g2 = Geeks('Bob')
+
+# Calling class methods
+print(Geeks.get_course())  
+print(Geeks.get_instance_count())  
+
+# Calling static method
+print(Geeks.welcome_message())
