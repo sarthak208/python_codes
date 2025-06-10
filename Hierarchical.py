c00465aa0f956620c7c62a6029fe8a156524c814
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-06-10 14:25:18 +0530

    Create Hierarchical.py

---
+++ Hierarchical.py
@@ -0,0 +1,13 @@
+class army: 
+def func1(self):
+print("the value of given term will 1st")
+class nsg(army):
+def func2(self):
+print("the value of given term will 2nd")
+class para(army): 
+def func3(self): 
+print("the value of given term will 3rd")
+object=para
+obj=input("enter the value of para")
+print(obj)
+object.func3()
