a4a4b711247b25cfc1da20b45a6abb0a25b9eb68
sarthak208 <140626574+sarthak208@users.noreply.github.com>
2025-06-10 14:22:24 +0530

    Create Hybrid.py

---
+++ Hybrid.py
@@ -0,0 +1,16 @@
+class army:
+def funcl(self):
+print("the value of given term will ist")
+class nsg(army): 
+def func2(self):
+print("the value of given term will 2nd")
+class paratnsg);
+def func3(self):
+print("the value of given term will 3rd*)
+class cobra(army): 
+def func4(self): 
+print("the value of given term will be 4th')
+oj.func4()
+obj=cobra()
+obj=input("enter the value of cobra)
+print(object)
